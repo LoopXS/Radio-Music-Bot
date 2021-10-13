8caced22561c0e9b0041486b45f2f1d055a409f3
LoopXS <91501744+LoopXS@users.noreply.github.com>
2021-10-13 09:15:51 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/Radio-Music-Bot.git /Radio-Music-Bot
+git clone https://github.com/LoopXS/Radio-Music-Bot.git /Radio-Music-Bot
 cd /Radio-Music-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
